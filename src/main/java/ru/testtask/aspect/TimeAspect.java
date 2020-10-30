605b0064a876f1a8d7b13ee3d4e888a2f7405074
Ivan Serdyuk <vanyaserdyuk1@gmail.com>
2020-10-30 14:53:59 +0300

    Revert "stopwatch timer"
    This reverts commit 76d989be4123d1fd07b969d86670d16f1dba8024.

--- src/main/java/ru/testtask/aspect/TimeAspect.java
+++ src/main/java/ru/testtask/aspect/TimeAspect.java
@@ -7,7 +7,6 @@ import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StopWatch;
 
 @Aspect
 @Component
@@ -22,13 +21,12 @@ public class TimeAspect {
 
     @Around("selectMethods()")
     public Object executionTime(ProceedingJoinPoint point) throws Throwable {
-        StopWatch stopWatch = new StopWatch();
-        stopWatch.start();
+        long startTime = System.currentTimeMillis();
         Object object = point.proceed();
-        stopWatch.stop();
+        long endtime = System.currentTimeMillis();
         log.info("Class name: {}. Method name: {}. Time taken for execution is {} ms"
                 ,point.getSignature().getDeclaringTypeName(),
-                point.getSignature().getName(), stopWatch.getLastTaskTimeMillis());
+                point.getSignature().getName(), (endtime-startTime));
         return object;
     }
 }
