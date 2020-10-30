76d989be4123d1fd07b969d86670d16f1dba8024
Ivan Serdyuk <vanyaserdyuk1@gmail.com>
2020-10-30 13:25:44 +0300

    stopwatch timer

--- src/main/java/ru/testtask/aspect/TimeAspect.java
+++ src/main/java/ru/testtask/aspect/TimeAspect.java
@@ -7,6 +7,7 @@ import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
 
 @Aspect
 @Component
@@ -21,12 +22,13 @@ public class TimeAspect {
 
     @Around("selectMethods()")
     public Object executionTime(ProceedingJoinPoint point) throws Throwable {
-        long startTime = System.currentTimeMillis();
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
         Object object = point.proceed();
-        long endtime = System.currentTimeMillis();
+        stopWatch.stop();
         log.info("Class name: {}. Method name: {}. Time taken for execution is {} ms"
                 ,point.getSignature().getDeclaringTypeName(),
-                point.getSignature().getName(), (endtime-startTime));
+                point.getSignature().getName(), stopWatch.getLastTaskTimeMillis());
         return object;
     }
 }
