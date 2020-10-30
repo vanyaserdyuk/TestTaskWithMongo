68c6f5d6ee3e77e30e4eaa5f38181de42c6ebe9c
Ivan Serdyuk <vanyaserdyuk1@gmail.com>
2020-10-30 13:46:18 +0300

    New test for controller checking(NPE)

--- src/test/java/ru/testtask/service/ProjectServiceTest.java
+++ src/test/java/ru/testtask/service/ProjectServiceTest.java
@@ -1,53 +1,34 @@
 package ru.testtask.service;
 
-import lombok.extern.slf4j.Slf4j;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringRunner;
-import ru.testtask.Application;
-import ru.testtask.config.TestConfig;
-import ru.testtask.exception.NameAlreadyExistsException;
+import org.mockito.junit.MockitoJUnitRunner;
 import ru.testtask.model.Attribute;
 import ru.testtask.model.Geometry;
 import ru.testtask.model.Project;
-
+import ru.testtask.service.ProjectService;
 import java.util.List;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
-import static org.junit.Assert.*;
 
-
-@Slf4j
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = {Application.class})
-@ContextConfiguration(classes = {TestConfig.class})
-@ActiveProfiles("test")
+@RunWith(MockitoJUnitRunner.class)
 public class ProjectServiceTest {
 
-    @Autowired
-    private ProjectService projectService;
+    private static ProjectService projectService;
 
-    @MockBean
-    private UserService userService;
-
-    private Project testProject;
+    private static Project testProject;
 
     @Before
     public void buildTestProject(){
         testProject = new Project();
-        testProject.setName("TestProject");
-
+        testProject.setName("Test");
     }
 
     @Before
-    public void clearDb(){
-        projectService.deleteAllProjects();
+    public void initService(){
+         projectService = new ProjectService();
     }
 
     @Test
@@ -66,19 +47,5 @@ public class ProjectServiceTest {
             assertNotNull(attribute.getName());
             assertNotNull(attribute.getId());
         }
-    }
-
-    @Test(expected = NameAlreadyExistsException.class)
-    public void checkSimilarNamesCreationTest(){
-        projectService.createProject(testProject);
-        projectService.createProject(testProject);
-    }
-
-    @Test
-    public void isCurrentUserOwnerOfTest(){
-        Mockito.when(userService.getCurrentUserId()).thenReturn("a");
-        projectService.createProject(testProject);
-        testProject.setOwnerId("a");
-        assertTrue(projectService.isCurrentUserOwnerOf(testProject.getId()));
     }
 }
