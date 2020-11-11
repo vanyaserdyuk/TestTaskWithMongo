7256215924caa3939e35e71f41932ce5994b2600
Ivan Serdyuk <vanyaserdyuk1@gmail.com>
2020-11-12 01:18:59 +0300

    Added new methods to UserController. Will be improved.

--- src/main/java/ru/testtask/config/WebSecurityConfig.java
+++ src/main/java/ru/testtask/config/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -13,6 +14,7 @@ import ru.testtask.service.UserService;
 
 @Configuration
 @EnableConfigurationProperties
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
