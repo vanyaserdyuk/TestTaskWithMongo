b9454678438dc25e400be63f5f7049bf1c2f123f
Ivan Serdyuk <vanyaserdyuk1@gmail.com>
2020-11-16 02:16:26 +0300

    Fixed some bugs and added tests

--- src/main/java/ru/testtask/dto/UserDTO.java
+++ src/main/java/ru/testtask/dto/UserDTO.java
@@ -5,12 +5,14 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import ru.testtask.model.Role;
 
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import java.util.Set;
 
 @Data
 @NoArgsConstructor
 public class UserDTO {
-    private String name;
+    private String username;
     private String password;
     @JsonDeserialize(as = Set.class)
     private Set<Role> roles;
