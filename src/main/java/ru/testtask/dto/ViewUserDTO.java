c29b8f910f1087fb2364e057203b1a226d0f8b83
Ivan Serdyuk <vanyaserdyuk1@gmail.com>
2020-11-16 19:44:32 +0300

    Fixed bugs and added new features

--- src/main/java/ru/testtask/dto/ViewUserDTO.java
+++ src/main/java/ru/testtask/dto/ViewUserDTO.java
@@ -1,20 +1,15 @@
 package ru.testtask.dto;
 
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import ru.testtask.model.Role;
 
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
 import java.util.Set;
 
 @Data
 @NoArgsConstructor
-public class UserDTO {
+public class ViewUserDTO {
     private String id;
     private String username;
-    private String password;
-    @JsonDeserialize(as = Set.class)
     private Set<Role> roles;
 }
