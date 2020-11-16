271cfd266688baeb84b946c9ee6348ea92a94193
Ivan Serdyuk <vanyaserdyuk1@gmail.com>
2020-11-17 02:18:09 +0300

    A few changes in tests

--- src/test/java/ru/testtask/controller/UserControllerTest.java
+++ src/test/java/ru/testtask/controller/UserControllerTest.java
@@ -6,31 +6,31 @@ import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import ru.testtask.model.Project;
 import ru.testtask.model.Role;
 import ru.testtask.model.User;
 import ru.testtask.repo.ProjectRepo;
-import ru.testtask.service.ProjectService;
 import ru.testtask.service.UserService;
 
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Optional;
 
-
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
+@WithUserDetails(value = "vanya")
 public class UserControllerTest {
 
     @Autowired
@@ -58,7 +58,8 @@ public class UserControllerTest {
                         .content(objectMapper.writeValueAsString(user))
                         .contentType(MediaType.APPLICATION_JSON)
         )
-                .andExpect(status().isCreated());
+                .andExpect(status().isCreated())
+                .andExpect(authenticated());
     }
 
 
@@ -75,7 +76,8 @@ public class UserControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.id").value("a"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.username").value("user"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.roles").value("USER"));
+                .andExpect(MockMvcResultMatchers.jsonPath("$.roles").value("USER"))
+                .andExpect(authenticated());
     }
 
     @Test
@@ -84,7 +86,8 @@ public class UserControllerTest {
                 thenReturn(Optional.empty());
         mockMvc.perform(
                 MockMvcRequestBuilders.get("/api/users/a"))
-                .andExpect(status().isNotFound());
+                .andExpect(status().isNotFound())
+                .andExpect(authenticated());
     }
 
     @Test
@@ -100,7 +103,8 @@ public class UserControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.id").value("a"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("user"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.roles").value("USER"));
+                .andExpect(MockMvcResultMatchers.jsonPath("$.roles").value("USER"))
+                .andExpect(authenticated());
     }
 
     @Test
@@ -111,23 +115,25 @@ public class UserControllerTest {
         Mockito.when(userService.getUserById(Mockito.anyString())).thenReturn(Optional.of(user));
         mockMvc.perform(
                 MockMvcRequestBuilders.delete("/api/users/a"))
-                .andExpect(status().isNoContent());
+                .andExpect(status().isNoContent())
+                .andExpect(authenticated());
+    }
+
+
+    @Test
+    public void getAllUsersTest() throws Exception {
+        User user = User.builder().id("a").username("user")
+                .roles(Collections.singleton(Role.USER)).build();
+
+        User admin = User.builder().id("b").username("admin")
+                .roles(Collections.singleton(Role.ADMIN)).build();
+
+        Mockito.when(userService.getAllUsers()).thenReturn(Arrays.asList(user, admin));
+        mockMvc.perform(
+                MockMvcRequestBuilders.get("/api/projects/"))
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(Arrays.asList(user, admin))))
+                .andExpect(authenticated());
+
     }
 }
-
-//    @Test
-//    public void getAllUsersTest() throws Exception {
-//        User user = User.builder().id("a").username("user")
-//                .roles(Collections.singleton(Role.USER)).build();
-//
-//        User admin = User.builder().id("b").username("admin")
-//                .roles(Collections.singleton(Role.ADMIN)).build();
-//
-//        Mockito.when(userService.getAllUsers().thenReturn(Arrays.asList(user, admin)));
-//        mockMvc.perform(
-//                MockMvcRequestBuilders.get("/api/projects/"))
-//                .andExpect(status().isOk())
-//                .andExpect(content().json(objectMapper.writeValueAsString(Arrays.asList(user, admin))));
-//
-//    }
-//}
