cf452dec4b8cf657230bfbae37351b78edffb278
Ivan Serdyuk <vanyaserdyuk1@gmail.com>
2020-12-11 03:01:59 +0300

    #13 Websocket

--- src/main/java/ru/testtask/controller/ChatWsController.java
+++ src/main/java/ru/testtask/controller/ChatWsController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import ru.testtask.dto.ChatMessageUserDTO;
 import ru.testtask.service.ChatMessageServiceImpl;
 
+import static ru.testtask.service.ChatMessageServiceImpl.WS_SESSION_ATTRIBUTE_ROOM_ID;
 import static ru.testtask.service.ChatMessageServiceImpl.WS_SESSION_ATTRIBUTE_USERNAME;
 
 
@@ -29,6 +30,10 @@ public class ChatWsController {
     @MessageMapping("/ws/chat/room/{roomId}/user/add")
     public void addUserToRoom(@DestinationVariable String roomId, @Payload String username,
                               SimpMessageHeaderAccessor headerAccessor) {
+        String currentRoomId = (String) headerAccessor.getSessionAttributes().put(WS_SESSION_ATTRIBUTE_ROOM_ID, roomId);
+        if (currentRoomId != null) {
+            chatMessageServiceImpl.sendUserLeaveFromRoom(roomId, username);
+        }
         headerAccessor.getSessionAttributes().put(WS_SESSION_ATTRIBUTE_USERNAME, username);
         chatMessageServiceImpl.sendUserJoinToRoom(roomId, username);
     }
