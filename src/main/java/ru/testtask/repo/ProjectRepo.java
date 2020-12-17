341e862ecfbed3a7df1dffaa1862a96fd470ab5f
vanyaserdyuk <70968006+vanyaserdyuk@users.noreply.github.com>
2020-12-17 10:34:50 +0300

    13-file-manager (#28)
     #13 File Manager

--- src/main/java/ru/testtask/repo/ProjectRepo.java
+++ src/main/java/ru/testtask/repo/ProjectRepo.java
@@ -1,12 +1,8 @@
 package ru.testtask.repo;
 
-
-
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 import ru.testtask.model.Project;
-
-
 
 @Repository
 public interface ProjectRepo extends MongoRepository<Project, String> {
